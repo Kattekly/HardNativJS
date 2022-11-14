15ccbf5d2e3f13db5c575232f4352fc9bf80b20d
Ekaterina <kattekly@mail.ru>
2022-11-14 14:42:36 +0300

    Lessons-1

--- src/lessons/lesson2/lesson2.ts
+++ src/lessons/lesson2/lesson2.ts
@@ -107,6 +107,19 @@ function fib(n: number): number {
 
 console.log(fib(7))
 
+
+//
+function fib2(n: number): number {
+    let a = 1;
+    let b = 1;
+    for (let i = 3; i <= n; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+
 // Task 06
 // написать функцию, которая повторяет функционал метода flat массива на всю глубину.
 
