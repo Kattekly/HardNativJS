580058984b87a38898bb1404535ff5f0f0d8f708
Ekaterina <kattekly@mail.ru>
2023-01-10 16:47:59 +0300

    sobes

--- src/my/sobes.js
+++ src/my/sobes.js
@@ -20,3 +20,17 @@ const a = 12
 function b() {
     console.log(a)
 }
+
+//this в глобальном скоупе - глобальный объект
+console.log(this === window)
+
+//дополнительные примеры
+function fnA() {
+    console.log(this); // window (не строгий режим)
+}
+function fnB() {
+    'use strict';
+    console.log(this); // undefined (строгий режим)
+}
+fnA();
+fnB();
