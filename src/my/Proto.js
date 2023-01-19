d5f0da20dde28465aff12d260c238f8a1bec2f9e
Ekaterina <kattekly@mail.ru>
2023-01-19 14:22:48 +0300

    Proto and prototype

--- src/my/Proto.js
+++ src/my/Proto.js
@@ -101,3 +101,27 @@ const Kate = {
     name: "Kate",
     __proto__: Super
 }
+
+//==============================
+let head = {
+    glasses: 1
+};
+
+let table = {
+    pen: 3,
+    __proto__: head
+};
+
+let bed = {
+    sheet: 1,
+    pillow: 2,
+    __proto__: table
+};
+
+let pockets = {
+    money: 2000,
+    __proto__: bed
+};
+
+console.log(pockets.pen)
+console.log(bed.glasses)
